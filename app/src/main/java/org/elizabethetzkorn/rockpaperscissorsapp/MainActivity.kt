d466cae0cd48638e2729dae6ab6c4aeb62aa10dd
ElizabethEtzkorn <elizabeth.etzkorn@smail.astate.edu>
2019-11-17 13:29:38 -0600

    added comment

---
+++ app/src/main/java/org/elizabethetzkorn/rockpaperscissorsapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package org.elizabethetzkorn.rockpaperscissorsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    //changes with comment
+}
